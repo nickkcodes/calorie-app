db190c351b97d2c9b48e9d2f4cca76677a6dd588
Nick <mayumicalamno5@gmail.com>
2026-04-23 21:13:50 +0800

    feat: Dark mode WIP

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+const themeToggle = document.getElementById('theme-toggle');
+
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    themeToggle.textContent = currentTheme === 'dark' ? '☀️' : '🌙';
+}
+
+themeToggle.addEventListener('click', () => {
+    let theme = document.documentElement.getAttribute('data-theme');
+    
+    if (theme === 'dark') {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+        themeToggle.textContent = '🌙';
+    } else {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+        themeToggle.textContent = '☀️';
+    }
+});
+
 let totalCalories = 0
 let calorieGoal   = 2000
 let glasses       = 0
